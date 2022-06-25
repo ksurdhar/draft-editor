60946a3f9448d94fffa58d9dc7fd043b7b607f46
Kiran Surdhar <ksurdhar1@gmail.com>
2022-06-25 13:00:54 -0600

    updated saved / unsaved status text in editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -14,20 +14,20 @@ const Editor = ({ documentText, documentId }: EditorProps) => {
 
   const debouncedSave = useDebouncedCallback(
     async () => { 
-      setIsUpdated(false)
       await API.patch(`documents/${documentId}`, { content: text }) 
       setIsUpdated(true)
     }, 1000
   )
 
   const handleChange = (e: React.SyntheticEvent<HTMLTextAreaElement>) => {
+    setIsUpdated(false)
     setText(e.currentTarget.value)
     debouncedSave()
   }
 
   return (
     <div className='flex-grow'>
-      { !isUpdated ? <span>updating</span> : <span>up to date</span>}
+      { !isUpdated ? <span>Unsaved Changes</span> : <span>Saved</span>}
       <textarea className='border-solid border-2 border-slate-300 rounded-md w-full h-full' value={text} onChange={handleChange} />
     </div>
   )
