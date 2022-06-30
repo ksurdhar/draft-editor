3763cc3cf995cfa49518cea98b13cc91bbc16620
Kiran Surdhar <ksurdhar1@gmail.com>
2022-06-30 12:14:29 -0600

    added slate js to contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,15 +1,40 @@
-import Link from "next/link"
+import { useState } from 'react'
+import { createEditor, BaseEditor, Descendant } from 'slate'
+import { Slate, Editable, withReact, ReactEditor } from 'slate-react'
+
 import Layout from "../../components/layout"
 
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue: Descendant[] = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'this is the slate component!' }],
+  },
+]
+
 const ContactPage = () => {  
+  const [ editor ] = useState(() => withReact(createEditor()))
+
   return (
     <Layout>
       <h1>Contact</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+      <Slate editor={editor} value={initialValue}>
+        <Editable 
+          onKeyDown={event => {
+            console.log(event.key)
+          }}
+        />
+      </Slate>
     </Layout>
   )
 }
