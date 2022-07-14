86bab0762d83cab7cd35776caf548529924ef139
Kiran Surdhar <ksurdhar1@gmail.com>
2022-07-14 12:13:36 -1000

    improved drawer menu styles

--- components/header.tsx
+++ components/header.tsx
@@ -2,13 +2,9 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useUser } from '@auth0/nextjs-auth0'
 import { useState } from 'react'
-import { Menu, Transition } from '@headlessui/react'
 
 import API from '../lib/utils'
 
-
-{/* <span className='pr-4'>Hi, Kiran</span> */}
-{/* {user && <span>Hi, ${user.name}</span>} */}
 
 const HeaderComponent = () => {
   const router = useRouter()
@@ -19,64 +15,44 @@ const HeaderComponent = () => {
     <header className="flex flex-row p-5 max-h-16 justify-between bg-transparent">
       <h1 className='lowercase'><Link href={'/'}>Whetstone</Link></h1>
       <div>
-        <Menu>
-          <div className='flex flex-row-reverse z-10 absolute right-[20px]'>
-            <Menu.Button onClick={() => setMenuOpen(!menuOpen)} className={`hamburger hamburger--spin ${ menuOpen ? 'is-active' : ''}`}>
+          <div className='flex flex-row-reverse z-20 absolute right-[20px]'>
+            <div onClick={() => setMenuOpen(!menuOpen)} className={`hamburger hamburger--spin ${ menuOpen ? 'is-active' : ''}`}>
               <span className="hamburger-box">
                 <span className="hamburger-inner"></span>
               </span>
-            </Menu.Button>
+            </div>
           </div>
-          <Transition
-            enter="transition duration-500 ease-in"
-            enterFrom="transform opacity-0"
-            enterTo="transform opacity-100"
-            leave="transition duration-500 ease-out"
-            leaveFrom="transform opacity-100"
-            leaveTo="transform opacity-0"
-            className={'absolute top-0 right-0'}
-          >
-          <Menu.Items static className={'h-[100vh] w-[30vw] min-w-[300px] menu-gradient p-[10px] pt-[48px] text-[20px] text-white'}>
-            <Menu.Item>
-              {({ active }) => (
-                <div onClick={async () => {
-                  setMenuOpen(!menuOpen)
-                  try {
-                    const res = await API.post(`/api/documents`, { title: 'Document with my Id', userId: user?.sub })
-                    const documentId = res.data.id
-                    router.push(`/documents/${documentId}`)
-                  } catch (e) {
-                    console.log(e)
-                  }
-                }} className={'hover:bg-white/[.3] cursor-pointer p-2 px-[10px]'}>
-                  Create Document
-                </div>
-                )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <div onClick={() => {
-                  router.push('/documents')
-                  setMenuOpen(!menuOpen)
-                }} className={'hover:bg-white/[.3] cursor-pointer p-2 px-[10px]'}>
-                  All Documents
-                </div>
-                )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <div className={'hover:bg-white/[.3] cursor-pointer p-2 px-[10px]'}>
-                  <Link href='/api/auth/logout'>
-                    <a>Sign Out</a>
-                  </Link>
-                </div>
-                )}
-            </Menu.Item>
-          </Menu.Items>
-          </Transition>
-        </Menu>
-      </div>
-        
+
+          <div className={`${router.pathname === '/documents' ? 'bg-menu' : 'bg-menu-dark'} transition-[right] ease-in-out duration-500 absolute top-0 ${menuOpen ? 'right-0' : 'right-[-500px]'} h-[100vh] min-w-[300px] 
+            p-[10px] pt-[48px] text-[20px] text-white z-10`}>
+            <div className={'hover:bg-white/[.3] cursor-pointer p-2 px-[20px] pl-[26px]'}
+              onClick={async () => {
+                setMenuOpen(!menuOpen)
+                try {
+                  const res = await API.post(`/api/documents`, { title: 'Document with my Id', userId: user?.sub })
+                  const documentId = res.data.id
+                  router.push(`/documents/${documentId}`)
+                } catch (e) {
+                  console.log(e)
+                }
+              }}>
+              Create Document
+            </div>
+            <div className={'hover:bg-white/[.3] cursor-pointer p-2 px-[20px] pl-[26px]'}
+              onClick={() => {
+                router.push('/documents')
+                setMenuOpen(!menuOpen)
+              }}>
+              All Documents
+            </div>
+            <div className={'hover:bg-white/[.3] cursor-pointer p-2 px-[20px] pl-[26px]'}>
+              <Link href='/api/auth/logout'>
+                <a>Sign Out</a>
+              </Link>
+            </div>
+          </div>
+
+        </div>
     </header>
   )
 }
--- pages/documents/[id].tsx
+++ pages/documents/[id].tsx
@@ -43,16 +43,10 @@ export default function DocumentPage({ id }: InferGetStaticPropsType<typeof getS
     }
     setTimeout(() => {
       setEditorColor(true)
-    }, 500)
+    }, 250)
   }, [isLoading])
 
   const { data: document } = useSWR<DocumentData>(`/api/documents/${id}`, fetcher,  { refreshInterval: 1000 }) 
-
-  if (!document) return (
-    <Layout>
-      <div>rendering</div>
-    </Layout>
-  )
 
   return (
     <>
@@ -64,11 +58,12 @@ export default function DocumentPage({ id }: InferGetStaticPropsType<typeof getS
       </Head>
       <Layout>
 
-      <div className={`transition-opacity ease-in-out duration-[1800ms] gradient ${editorColor ? 'opacity-0' : 'opacity-100' }  fixed top-0 left-0 h-screen w-screen z-[-1]`}/>
-      <div className={`transition-opacity ease-in-out duration-[1800ms] gradient-editor ${editorColor ? 'opacity-100' : 'opacity-0' }  fixed top-0 left-0 h-screen w-screen z-[-1]`}/>
+      <div className={`transition-opacity ease-in-out duration-[3000ms] gradient ${editorColor ? 'opacity-0' : 'opacity-100' }  fixed top-0 left-0 h-screen w-screen z-[-1]`}/>
+      <div className={`transition-opacity ease-in-out duration-[3000ms] gradient-editor ${editorColor ? 'opacity-100' : 'opacity-0' }  fixed top-0 left-0 h-screen w-screen z-[-1]`}/>
       <div className="flex justify-center pb-10 p-[20px] mt-[64px] text-black/[.79] font-editor2">
         <div className="flex flex-col h-[calc(100vh_-_64px)] pb-10 min-w-[calc(100vw_-_40px)] md:min-w-[0px] max-w-[740px] md:w-[740px]">
-          <Editor id={id} text={JSON.parse(document.content)} title={document.title} />
+          { document && <Editor id={id} text={JSON.parse(document.content)} title={document.title} /> }
+          { !document && 'rendering' }
         </div>
       </div>
     </Layout> 
