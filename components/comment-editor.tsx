4305254d1b6d6a1911c016c230a4fc4ac72c26d1
Kiran Surdhar <ksurdhar1@gmail.com>
2022-09-22 15:05:37 -0600

    added min width and auto focusing

--- components/comment-editor.tsx
+++ components/comment-editor.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useRef, useState } from 'react'
 import { createEditor, Descendant } from 'slate'
-import { withHistory } from 'slate-history'
 import { Slate, Editable, withReact } from 'slate-react'
 import { renderElement, renderLeaf } from './editor'
 
 const CommentEditor = () => {
-  const [ editor ] = useState(() => withReact(withHistory(createEditor())))
-  const [ text, setText ] = useState<Descendant[]>([{ type: 'default', children: [{text: 'hello world', highlight: 'none'}]}])
+  const [ editor ] = useState(() => withReact(createEditor()))
+  const [ text, setText ] = useState<Descendant[]>([{ type: 'default', children: [{text: '', highlight: 'none'}]}])
   const containerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    containerRef?.current?.focus()
+    setTimeout(() => {
+      console.log(containerRef.current)
+      const containerNode = containerRef.current
+      const documentNode = containerNode?.querySelector<HTMLTextAreaElement>(`[data-slate-editor="true"]`)
+      documentNode?.focus()
+    }, 0)
   }, [])
 
   return (
     <div className={`flex flex-1 flex-col h-[75vh] justify-center m-[10px]`}>
-      <div className={`fixed p-10 bg-slate-400`} ref={containerRef}>
+      <div className={`fixed p-10 bg-slate-400 min-w-[100px]`} ref={containerRef}>
         <Slate editor={editor} value={text} 
           onChange={value => {
             const isAstChange = editor.operations.some(op => 'set_selection' !== op.type)
@@ -31,7 +35,7 @@ const CommentEditor = () => {
             renderElement={renderElement}
             renderLeaf={renderLeaf}
             onKeyDown={event => {
-              console.log('onkeydown', event.currentTarget)
+              // console.log('onkeydown', event.currentTarget)
             }}
           />
         </Slate>
