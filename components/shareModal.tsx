78ad3b239d9faa1c3275194e577b67bc5e3221f7
Kiran Surdhar <ksurdhar1@gmail.com>
2022-12-30 12:41:35 -1000

    added the option to add and remove emails

--- components/shareModal.tsx
+++ components/shareModal.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, B
 import { useState } from 'react'
 import { DocumentData } from '../types/globals'
 import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 interface ShareModalProps {
   open: boolean
@@ -26,16 +27,17 @@ const ShareModal = ({ open, onClose, document }: ShareModalProps) => {
   // have the ability to click to remove
   // only makes api request on save
 
-  const [ users, setUsers ] = useState<ShareUser[]>()
+  const [ users, setUsers ] = useState<ShareUser[]>([])
   const [ permission, setPermission ] = useState<Permission>(Permission.View)
+  const [ email, setEmail] = useState<string>('')
 
   return (
     <Dialog open={open} onClose={onClose}>
+      <Box sx={{ minWidth: '576px' }}>
       <DialogTitle>{`Share ${document.title}`}</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          To subscribe to this website, please enter your email address here. We
-          will send updates occasionally.
+          Add or remove users who have access to this document.
         </DialogContentText>
         <Box sx={{ display: 'flex', alignItems: 'center', padding: '16px' }}>
           <TextField
@@ -47,9 +49,12 @@ const ShareModal = ({ open, onClose, document }: ShareModalProps) => {
               '& input:focus': { boxShadow: 'none', outline: 'none' },
               '& input': { height: '40px' } 
             }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <FormControl sx={{ marginRight: '12px' }}>
             <Select
+              sx={{ width: '131px' }}
               value={permission}
               onChange={(e) => setPermission(e.target.value as Permission)}
             >
@@ -58,15 +63,37 @@ const ShareModal = ({ open, onClose, document }: ShareModalProps) => {
               <MenuItem value={Permission.Edit}>Editor</MenuItem>
             </Select>
           </FormControl>
-          <IconButton color="primary" aria-label="add to shopping cart">
+          <IconButton 
+            onClick={() => {
+              setUsers(users.concat([{ email, permission }]))
+            }}
+            color="primary">
             <AddIcon />
           </IconButton>
         </Box>
+        <Box>
+          { users.map((user) => {
+            return (
+              <Box key={user.email}>
+                <span className='mr-4'>{user.email}</span>
+                <span className='mr-4'>{user.permission}</span>
+                <IconButton 
+                  onClick={() => {
+                    setUsers(users.filter((usr) => usr !== user))
+                  }}
+                  color="error">
+                  <RemoveIcon />
+                </IconButton>
+              </Box>
+            )
+          })}
+        </Box>
       </DialogContent>
       <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={onClose}>Subscribe</Button>
+        <Button onClick={onClose}>Copy Link</Button>
+        <Button onClick={onClose}>Done</Button>
       </DialogActions>
+      </Box>
     </Dialog>
   )
 }
