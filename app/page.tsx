719f970fb190c2565b45106d4accb0475a773d8d
Kiran Surdhar <ksurdhar1@gmail.com>
2023-10-20 13:55:00 -0600

    react code sharing working, no local fonts yet

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import { Container, Titles } from 'components/landing-page'
 import type { Metadata, NextPage } from 'next'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 export const metadata: Metadata = {
   title: 'whetstone',
@@ -8,21 +8,11 @@ export const metadata: Metadata = {
 
 const Home: NextPage = () => {
   return (
-    <div className={`${styles.container} font-index`}>
-      <div className='gradient absolute top-0 left-0 h-screen w-screen z-[-1]'/>
-      <main className={`${styles.main}`}>
+    <Container>
       <Link href="/documents">
-        <div className={'cursor-pointer'}>
-          <div className={`text-black/[.78] text-[78px]`}>
-            whetstone
-          </div>
-          <h2 className={`font-editor2 text-black/[.5] text-[30px]`}>
-            The writing app for editing.
-          </h2>
-        </div>
+        <Titles />
       </Link>
-      </main>
-    </div>
+    </Container>
   )
 }
 
