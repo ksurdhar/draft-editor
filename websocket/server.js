f389cff875cd7a9373e6c9ce45db1f0997247397
Kiran Surdhar <ksurdhar1@gmail.com>
2023-11-25 18:46:51 -0700

    added websocket server

---
+++ websocket/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+
+// Set up Express
+const app = express()
+const server = http.createServer(app)
+
+// Initialize Socket.IO
+const io = socketIo(server)
+
+// Handle a client connection
+io.on('connection', socket => {
+  const documentId = socket.handshake.query.documentId
+  socket.join(documentId)
+
+  console.log(`Client connected to room: ${documentId}`)
+
+  // Emit a message to the clients in the same room
+  socket.to(documentId).emit('joined', 'A new user has joined the document.')
+
+  // Handle messages sent from the client
+  socket.on('message', msg => {
+    console.log(`Message from client in room ${documentId}:`, msg)
+
+    // Broadcast the message to all clients in the same room
+    socket.to(documentId).emit('message', msg)
+  })
+
+  // Handle client disconnection
+  socket.on('disconnect', () => {
+    console.log(`Client disconnected from room: ${documentId}`)
+    socket.to(documentId).emit('left', 'A user has left the document.')
+  })
+})
+
+// Start the server
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
